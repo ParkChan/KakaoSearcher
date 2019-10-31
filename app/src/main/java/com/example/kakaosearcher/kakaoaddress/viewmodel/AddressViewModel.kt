539dcc764ecd450a5217e06cd02678bd0123db65
ParkChan <hydrarkk@naver.com>
2019-11-01 02:20:57 +0900

    [코드수정] private setter public getter property-names 의 Names for backing properties 참고

--- app/src/main/java/com/example/kakaosearcher/kakaoaddress/viewmodel/AddressViewModel.kt
+++ app/src/main/java/com/example/kakaosearcher/kakaoaddress/viewmodel/AddressViewModel.kt
@@ -1,17 +1,23 @@
 package com.example.kakaosearcher.kakaoaddress.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.kakaosearcher.common.BaseViewModel
-import com.example.kakaosearcher.kakaoaddress.model.response.AddressModel
 import com.example.kakaosearcher.kakaoaddress.model.dto.AddressDto
+import com.example.kakaosearcher.kakaoaddress.model.response.AddressModel
 import com.example.kakaosearcher.kakaoaddress.repository.AddressRepository
 import com.example.kakaosearcher.network.retrofit.CallBackListener
 
 
 class AddressViewModel(private val addressRepository: AddressRepository) : BaseViewModel() {
 
-    val addressList = MutableLiveData<List<AddressModel>>()
-    val throwable = MutableLiveData<Throwable>()
+    private val _addressList = MutableLiveData<List<AddressModel>>()
+    val addressList: LiveData<List<AddressModel>>
+        get() = _addressList
+
+    private val _throwable = MutableLiveData<Throwable>()
+    val throwable: LiveData<Throwable>
+        get() = _throwable
 
     private fun searchAddress(query: String) {
         compositeDisposable.add(
@@ -19,13 +25,11 @@ class AddressViewModel(private val addressRepository: AddressRepository) : BaseV
                 query,
                 object : CallBackListener<AddressDto> {
                     override fun onSuccess(responseData: AddressDto) {
-                        responseData.addressList?.apply {
-                            addressList.value = this
-                        }
+                        _addressList.value = responseData.addressList
                     }
 
                     override fun onFail(t: Throwable) {
-                        throwable.value = t
+                        _throwable.value = t
                     }
                 }
             )
